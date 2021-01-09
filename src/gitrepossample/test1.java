ce4d5a384c1ee75d63f61ddcbad4157b77aa5c33
Dinesh Reddy <Dinesh Reddy@Dinesh>
2021-01-09 14:42:51 +0530

    First commit

---
+++ src/gitrepossample/test1.java
@@ -0,0 +1,14 @@
+package gitrepossample;
+
+public class test1 {
+	public static void main(String[] args) {
+int a=9;
+if(a==9) {
+	System.out.println("R");
+}
+else {
+	System.out.println("W");
+}
+
+}
+}
